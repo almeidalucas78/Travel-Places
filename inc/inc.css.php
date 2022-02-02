a0e890cd443bfe91ac24880d3195e3b5874ef894
almeidalucas78 <lucas-silva-dealmeida123@hotmail.com>
2022-02-01 21:52:14 -0300

    subindo projeto

---
+++ inc/inc.css.php
@@ -0,0 +1,6 @@
+<!--CSS LINKS-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+
+<!--SLICK-->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Travel Places</title>
+  <?php include 'inc/inc.css.php' ?>
+</head>
+
+<body>
+  <!--HEADER-->
+  <section class="banner-area">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img src="img/logo-adventure.webp" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">FAQ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Pages</a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="your-class">
+      <div class="banner-home01">
+        <div class="banner-overlay">
+          <span>DISCOVER THE COLORFULL WORLD</span>
+          <h2>NEW ADVENTURE</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp.</br> Or incididunt ut labore et dolore magna aliqua. Ut enim ad minim. </p>
+          <a href="#" class="btn-banner">DISCOVER NOW</a>
+        </div>
+      </div>
+      <div class="banner-home02">
+        <div class="banner-overlay">
+          <span>DISCOVER THE COLORFULL WORLD</span>
+          <h2>NEW TRIP</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp.</br> Or incididunt ut labore et dolore magna aliqua. Ut enim ad minim. </p>
+          <a href="#" class="btn-banner">DISCOVER NOW</a>
+        </div>
+      </div>
+      <div class="banner-home03">
+        <div class="banner-overlay">
+          <span>DISCOVER THE COLORFULL WORLD</span>
+          <h2>NEW EXPERIENCE</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp.</br> Or incididunt ut labore et dolore magna aliqua. Ut enim ad minim. </p>
+          <a href="#" class="btn-banner">DISCOVER NOW</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include 'inc/inc.js.php' ?>
+
+  <main>
+    <section class="about-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h2>Why Choose Us Your Fitness Builder</h2>
+            <p>Who are in extremely love with eco friendly system.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="about-img">
+              <img src="img/about/about-img.webp" class="img-fluid" alt="">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="about-text-area">
+              <h3>We Realize that<br /> there are reduced <br />Wastege Stand out</h3>
+              <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women.</p>
+              <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women. inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women.</p>
+              <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+
+</body>
+
+</html>
